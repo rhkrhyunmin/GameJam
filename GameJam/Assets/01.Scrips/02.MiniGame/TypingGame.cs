74972c5ad90a4cea38ff74d6f7fe5f3304d3c953
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-06-03 10:41:20 +0900

    .

--- GameJam/Assets/01.Scrips/02.MiniGame/TypingGame.cs
+++ GameJam/Assets/01.Scrips/02.MiniGame/TypingGame.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TypingGame : MonoBehaviour
